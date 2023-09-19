b26fe23ba23591aea2162955866ee2b34bca463a
Nahason <nahason.murithi@student.moringaschool.com>
2023-09-19 10:05:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let greetingForFeet;
+  if (ride <= 400){
+    greetingForFeet = 'This one is on me!';
+    return greetingForFeet;
+  } else 
+  if (ride>=2000 && ride<=2500){
+    greetingForFeet = 'I will gladly take your thirty bucks.';
+    return greetingForFeet;
+  }else 
+  if (ride >= 2500){
+    greetingForFeet = 'No can do.';
+    return greetingForFeet;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let cityResponse = (city === 'NYC') ? "Ok, sounds good." : "No go.";
+  return cityResponse;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+  case 'generous':
+  response = "Thank you so much."
+  break;
+  case 'not as generous':
+  response = "Thank you."
+  break;
+  default:
+    response = "Bye."
+    break;
+  }
+  return response;
 }
